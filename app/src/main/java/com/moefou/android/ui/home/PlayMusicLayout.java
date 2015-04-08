2b952ad0573de2eb9735f9e39af700a1fe50a8fe
evan <liusuzheng2012@gmail.com>
2015-04-08 22:24:30 +0800

    touch

--- app/src/main/java/com/moefou/android/ui/home/PlayMusicLayout.java
+++ app/src/main/java/com/moefou/android/ui/home/PlayMusicLayout.java
@@ -2,12 +2,22 @@ package com.moefou.android.ui.home;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.moefou.android.R;
 
 public class PlayMusicLayout extends RelativeLayout {
+
+    private static final String TAG = "PlayMusicLayout";
+    private ViewGroup mActionGroup, mContent;
+    private ImageView mActionFavImage, mActionDeleteImage, mActionNextImage;
+
     public PlayMusicLayout(Context context) {
         this(context, null);
     }
@@ -20,6 +30,41 @@ public class PlayMusicLayout extends RelativeLayout {
         super(context, attrs, defStyleAttr);
 
         LayoutInflater.from(context).inflate(R.layout.play_music_layout, this);
+        mActionGroup = (ViewGroup) findViewById(R.id.action_layout);
+        mActionFavImage = (ImageView) findViewById(R.id.action_unfav);
+        mActionDeleteImage = (ImageView) findViewById(R.id.action_delete);
+        mActionNextImage = (ImageView) findViewById(R.id.action_next);
+        mContent = (ViewGroup) findViewById(R.id.content);
 
+        mActionFavImage.setEnabled(false);
+        mActionDeleteImage.setEnabled(false);
+        mActionNextImage.setEnabled(false);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float y = 0;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                y = event.getY();
+
+                Log.d(TAG, "y " + y);
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float offY = event.getY() - y;
+
+                Log.d(TAG, "event.getY() " + event.getY());
+//                mActionGroup.setLayoutParams(new LinearLayout.LayoutParams((int) offY, mActionGroup.getHeight()));
+
+                break;
+            case MotionEvent.ACTION_UP:
+
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
